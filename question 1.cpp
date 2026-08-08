3fa71ae660c2ae1d87cb2311e840d4102ff2aafc
lihasraj02-source <lihasraj02@gmail.com>
2026-08-08 14:54:52 +0530

    Add files via upload

---
+++ question 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){ 
+
+int arr[]={1,2,1,2,4}; int i; int x=0; 
+for(i=0;i<5;i++){
+ 
+x =x^arr[i]; 
+
+
+} cout<<x<<endl;
+
+return 0;
+}
+
